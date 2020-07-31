da6915973ef6e22e8b8383f1b6d9a606c4a6905f
Tanya-Goel <goeltanya01@gmail.com>
2020-07-31 12:34:34 +0530

    Updates

---
+++ precache-manifest.6623726baf7b9df3700e6eb5b886001d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77e44e4b902a7ccbf3dbecfa621b9ed7",
+    "url": "/netflix_series/index.html"
+  },
+  {
+    "revision": "d73e01891494d8274d4d",
+    "url": "/netflix_series/static/css/main.efcef0ae.chunk.css"
+  },
+  {
+    "revision": "b98b7a584d1c931f2cd8",
+    "url": "/netflix_series/static/js/2.da965c6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/netflix_series/static/js/2.da965c6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d73e01891494d8274d4d",
+    "url": "/netflix_series/static/js/main.63786793.chunk.js"
+  },
+  {
+    "revision": "07ecc065d6292ef8c356",
+    "url": "/netflix_series/static/js/runtime-main.447b650c.js"
+  }
+]);
